61c356ba74854121ba64a254e8138f63d026272b
Bence Webber <thawildcards@gmail.com>
2022-06-19 22:57:39 +0200

    Add basic test file

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// const { expect } = require("chai");
+
+// describe("Base ERC721", function () {
+//   it("Should assign owner when minted", async function () {
+//     const [owner] = await ethers.getSigners();
+
+//     const NFT = await ethers.getContractFactory("ERC721Lendable").deploy();;
+
+//     await hardhatToken.balanceOf(owner.address);
+//     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+//   });
+// });
