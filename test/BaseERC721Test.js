823ba77adb1ab35b0bee23c6f0c5acbe3fa50f35
Bence Webber <thawildcards@gmail.com>
2022-06-27 22:40:01 +0200

    Add basic approval test case

--- test/BaseERC721Test.js
+++ test/BaseERC721Test.js
@@ -29,4 +29,23 @@ describe("Base ERC721", function () {
     await NFT.transferFrom(ownerAddr, tester1Addr, 1);
     expect(await NFT.ownerOf(1)).to.equal(tester1Addr);
   });
+
+  it("Should allow transfer after approval", async function () {
+    const [owner, tester1, tester2] = await ethers.getSigners();
+    const [ownerAddr, tester1Addr, tester2Addr] =  await Promise.all([owner, tester1, tester2].map(async x => await x.getAddress()));
+
+    const NFTFactory = await ethers.getContractFactory("TestNFT");
+    const NFT = await NFTFactory.deploy();
+
+    await NFT.mint();
+
+    await expect(
+        NFT.connect(tester1).transferFrom(ownerAddr, tester2Addr, 1)
+    ).to.be.revertedWith("ERC721Lendable: transfer caller is not admin");
+    await NFT.approve(tester1Addr, 1);
+
+    expect(await NFT.ownerOf(1)).to.equal(ownerAddr);
+    await NFT.connect(tester1).transferFrom(ownerAddr, tester2Addr, 1);
+    expect(await NFT.ownerOf(1)).to.equal(tester2Addr);
+  });
 });
