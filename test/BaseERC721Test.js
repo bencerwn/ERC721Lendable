c1c6ea3caebddcc60c262aa01e606c5ece62cad1
Bence Webber <thawildcards@gmail.com>
2022-06-27 22:46:09 +0200

    Normalize indentation

--- test/BaseERC721Test.js
+++ test/BaseERC721Test.js
@@ -3,46 +3,48 @@ const { expect } = require("chai");
 
 
 describe("Base ERC721", function () {
-  it("Should assign owner when minted", async function () {
-    const [owner] = await ethers.getSigners();
+    
 
-    const NFTFactory = await ethers.getContractFactory("TestNFT");
-    const NFT = await NFTFactory.deploy();
+    it("Should assign owner when minted", async function () {
+        const [owner] = await ethers.getSigners();
 
-    await NFT.mint();
-    expect(await NFT.ownerOf(1)).to.equal(owner.address);
-    await NFT.mint();
-    expect(await NFT.ownerOf(2)).to.equal(owner.address);
-  });
+        const NFTFactory = await ethers.getContractFactory("TestNFT");
+        const NFT = await NFTFactory.deploy();
 
-  it("Should assign new owner on transfer", async function () {
-    const [owner, tester1] = await ethers.getSigners();
+        await NFT.mint();
+        expect(await NFT.ownerOf(1)).to.equal(owner.address);
+        await NFT.mint();
+        expect(await NFT.ownerOf(2)).to.equal(owner.address);
+    });
 
-    const NFTFactory = await ethers.getContractFactory("TestNFT");
-    const NFT = await NFTFactory.deploy();
+    it("Should assign new owner on transfer", async function () {
+        const [owner, tester1] = await ethers.getSigners();
 
-    await NFT.mint();
-    expect(await NFT.ownerOf(1)).to.equal(owner.address);
+        const NFTFactory = await ethers.getContractFactory("TestNFT");
+        const NFT = await NFTFactory.deploy();
 
-    await NFT.transferFrom(owner.address, tester1.address, 1);
-    expect(await NFT.ownerOf(1)).to.equal(tester1.address);
-  });
+        await NFT.mint();
+        expect(await NFT.ownerOf(1)).to.equal(owner.address);
 
-  it("Should allow transfer after approval", async function () {
-    const [owner, tester1, tester2] = await ethers.getSigners();
+        await NFT.transferFrom(owner.address, tester1.address, 1);
+        expect(await NFT.ownerOf(1)).to.equal(tester1.address);
+    });
 
-    const NFTFactory = await ethers.getContractFactory("TestNFT");
-    const NFT = await NFTFactory.deploy();
+    it("Should allow transfer after approval", async function () {
+        const [owner, tester1, tester2] = await ethers.getSigners();
 
-    await NFT.mint();
+        const NFTFactory = await ethers.getContractFactory("TestNFT");
+        const NFT = await NFTFactory.deploy();
 
-    await expect(
-        NFT.connect(tester1).transferFrom(owner.address, tester2.address, 1)
-    ).to.be.revertedWith("ERC721Lendable: transfer caller is not admin");
-    await NFT.approve(tester1.address, 1);
+        await NFT.mint();
 
-    expect(await NFT.ownerOf(1)).to.equal(owner.address);
-    await NFT.connect(tester1).transferFrom(owner.address, tester2.address, 1);
-    expect(await NFT.ownerOf(1)).to.equal(tester2.address);
-  });
+        await expect(
+            NFT.connect(tester1).transferFrom(owner.address, tester2.address, 1)
+        ).to.be.revertedWith("ERC721Lendable: transfer caller is not admin");
+        await NFT.approve(tester1.address, 1);
+
+        expect(await NFT.ownerOf(1)).to.equal(owner.address);
+        await NFT.connect(tester1).transferFrom(owner.address, tester2.address, 1);
+        expect(await NFT.ownerOf(1)).to.equal(tester2.address);
+    });
 });
