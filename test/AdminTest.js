655be8b3c45ac21d1e5fc4677c4a09094a91c6a3
Bence Webber <thawildcards@gmail.com>
2022-06-30 13:16:36 +0200

    Add allow admin transfer test

--- test/AdminTest.js
+++ test/AdminTest.js
@@ -24,4 +24,13 @@ describe("Admin Test", function () {
         expect(await NFT.ownerOf(1)).to.equal(owner.address);
         expect(await NFT.adminOf(1)).to.equal(tester1.address);
     });
+
+    it("Should allow admin to transfer", async function () {
+      await NFT.mint();
+      await NFT.setAdmin(tester1.address, 1);
+
+      await NFT.connect(tester1).transferFrom(owner.address, tester2.address, 1);
+      expect(await NFT.ownerOf(1)).to.equal(tester2.address);
+      expect(await NFT.adminOf(1)).to.equal(tester1.address);
+  });
 });
