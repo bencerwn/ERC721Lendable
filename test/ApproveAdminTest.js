f3cf25d088f61b808d7da4971b5cbbb52646640f
Bence Webber <thawildcards@gmail.com>
2022-07-02 15:19:08 +0200

    Add allow operator to set admin test to approve admin test

--- test/ApproveAdminTest.js
+++ test/ApproveAdminTest.js
@@ -16,7 +16,15 @@ describe("Approve Admin Test", function () {
     });
 
 
-    it("", async function () {
+    it("Should allow operator to set admin", async function () {
         await NFT.mint();
+
+        await expect(
+          NFT.connect(tester1).setAdmin(tester2.address, 1)
+        ).to.be.revertedWith("ERC721Lendable: set admin caller is not admin");
+        await NFT.setApprovalForAll(tester1.address, true);
+
+        await NFT.connect(tester1).setAdmin(tester2.address, 1);  
+        expect(await NFT.adminOf(1)).to.equal(tester2.address);
     });
 });
