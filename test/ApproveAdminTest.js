047f96561d8728a07bec7436a4564f13ce15153f
Bence Webber <thawildcards@gmail.com>
2022-07-02 14:36:28 +0200

    Add approve admin test

---
+++ test/ApproveAdminTest.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+
+describe("Approve Admin Test", function () {
+    let owner;
+    let tester1;
+    let tester2;
+    let NFTFactory;
+    let NFT;
+    
+    beforeEach(async function () {
+        [owner, tester1, tester2] = await ethers.getSigners();
+        NFTFactory = await ethers.getContractFactory("TestNFT");
+        NFT = await NFTFactory.deploy();
+    });
+
+
+    it("", async function () {
+        await NFT.mint();
+    });
+});
